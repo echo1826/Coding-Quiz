8877ff394757b2059b7dced21b80aaa49e00934f
Ethan C <ethan.cho1826@gmail.com>
2021-09-28 21:00:35 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,161 @@ const questions = [{
     answer: "var"
 }];
 
+// begins to display current question user is on and buttons with answer choices
+function generateQuestion() {
+    console.log("Question generates")
+    // calls function to clear existing buttons if any
+    resetArea();
+    
+    let currentQuestion = questions[questionIndex];
 
+    // access the array of objects to get the question and write it to page
+    quizQuestion.textContent = questions[questionIndex].title;
+
+    // generates 4 buttons with answer choices written on them
+    for (let j = 0; j <= 3; j++) {
+        let tempButton = document.createElement("button");
+
+        let dataValue = currentQuestion.choices[j];
+
+        tempButton.setAttribute("value", dataValue);
+        // let dataTest = tempButton.getAttribute("value");
+
+        tempButton.textContent = currentQuestion.choices[j];
+
+        quizChoices.appendChild(tempButton);
+
+        // eventlistener added to buttons for when user clicks on them
+        tempButton.addEventListener("click", validateQuestion);
+    }
+    
+}
+
+
+// determines whether the user choice was the right answer
+function validateQuestion() {
+    // console.log(this.value);
+    if (this.value == questions[questionIndex].answer) {
+        amountRight++;
+        console.log(this);
+        rootEl.style.backgroundColor = "green"; // changes screen background color to green if right
+        console.log(amountRight);
+        questionIndex++;
+        if (questionIndex === questions.length) { // if no more questions go to show the results 
+            console.log("show results 2 happening");
+            showResults();
+            //return;
+        } else {
+            generateQuestion(); // generate another question
+        }
+
+    } else {
+        questionIndex++;
+        timeLeft -= 5;
+        rootEl.style.backgroundColor = "red"; // background color to red if wrong
+        if (timeLeft > 0) {
+            let timeInterval = setInterval(function () { // update timer if user answers incorrectly
+                // As long as the `timeLeft` is greater than 1
+                if (timeLeft > 1) {
+                    // Set the `textContent` of `timerDiv` to show the remaining seconds
+                    timerDiv.textContent = timeLeft + ' seconds remaining';
+                } else if (timeLeft === 1) {
+                    // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+                    timerDiv.textContent = timeLeft + ' second remaining';
+                }
+            }, 1000);
+            if (questionIndex === questions.length) {
+                console.log("show results 2 happening");
+                timeLeft = null;
+                showResults();
+                //return;
+
+            } else {;
+                generateQuestion();
+            }
+
+        } else {
+            timeLeft = null;
+            showResults();
+        }
+    }
+
+}
+// shows user the amount of questions they answered correctly
+function showResults() {
+    resetArea();
+    timerDiv.textContent = "";
+    timeLeft = null;
+    quizQuestion.textContent = "You got " + amountRight + " questions right!";
+    storeResults();
+
+}
+// gets rid of answer choice buttons
+function resetArea() {
+    //console.log("Clearing quiz area for next question")
+    while (quizChoices.firstChild) {
+        quizChoices.removeChild(quizChoices.firstChild);
+    }
+}
+// stores the amount of questions the user got right along with their submitted initials
+function storeResults() {
+    console.log("preparing to store locally");
+    let submitButton = document.createElement("button");
+    let submitText = document.createElement("input");
+
+    submitButton.textContent = "submit";
+
+    submitInitials.appendChild(submitText);
+    submitInitials.appendChild(submitButton);
+
+    // refreshes the entire page
+    refreshPage();
+    // when user hits submits, stores the initials and number of questions right into local storage
+    submitButton.addEventListener("click", function () {
+        console.log("storing locally now");
+        let submitName = submitText.value.trim();
+        if(amountRight > highRight) {
+            let highName = submitName;
+            let highRight = amountRight;
+        }       
+        const userScores = {
+            initials: submitName,
+            rightQuestion: amountRight
+        };
+        console.log(submitName);
+        console.log(amountRight);
+        localStorage.setItem("1", JSON.stringify(userScores));
+        submitInitials.remove(); // removes the input box after submission
+    });
+}
+// shows the score stored in local storage
+function displayScores() {
+    highScoreBtn.style.display = "none"; // hides button
+    let headerScore = document.createElement("h2");
+    highScoreScreen.appendChild(headerScore);
+    headerScore.textContent = "High Scores";
+    let scoresBox = document.createElement("ul");
+    highScoreScreen.appendChild(scoresBox);
+    let scores = document.createElement("li");
+    scoresBox.appendChild(scores);
+    let scoreObject = JSON.parse(localStorage.getItem("1")); // gets the initials and questions right from local storage
+    //console.log(scoreObject);
+    scores.textContent = scoreObject.initials + ": " + scoreObject.rightQuestion + " questions right";
+}
+// refreshes the page on clicking the start again button
+function refreshPage() {
+    refreshButton = document.createElement("button");
+    refreshButton.textContent = "start again";
+    timerDiv.appendChild(refreshButton)
+    refreshButton.addEventListener("click", function() {
+        window.location.reload();
+    })
+}
+
+// shows the score stored in local storage currently
+highScoreBtn.addEventListener("click", displayScores);
+
+// starts timer when start button is clicked
 startBtn.addEventListener("click", function () {
     console.log("Timer starts")
     let h1El = document.getElementById("starto");
@@ -65,153 +219,3 @@ startBtn.addEventListener("click", function () {
     generateQuestion();
 
 });
-
-highScoreBtn.addEventListener("click", displayScores);
-
-function generateQuestion() {
-    console.log("Question generates")
-    
-    resetArea();
-    
-    let currentQuestion = questions[questionIndex];
-
-    //quizQuestion.textContent = currentQuestion.title;
-    quizQuestion.textContent = questions[questionIndex].title;
-
-
-    for (let j = 0; j <= 3; j++) {
-        let tempButton = document.createElement("button");
-
-        let dataValue = currentQuestion.choices[j];
-
-        tempButton.setAttribute("value", dataValue);
-        // let dataTest = tempButton.getAttribute("value");
-
-        tempButton.textContent = currentQuestion.choices[j];
-
-        quizChoices.appendChild(tempButton);
-
-        tempButton.addEventListener("click", validateQuestion);
-    }
-    
-}
-
-
-
-function validateQuestion() {
-    // console.log(this.value);
-    if (this.value == questions[questionIndex].answer) {
-        amountRight++;
-        console.log(this);
-        rootEl.style.backgroundColor = "green";
-        console.log(amountRight);
-        questionIndex++;
-        if (questionIndex === questions.length) {
-            console.log("show results 2 happening");
-            showResults();
-            //return;
-        } else {
-            generateQuestion();
-        }
-
-    } else {
-        questionIndex++;
-        timeLeft -= 5;
-        rootEl.style.backgroundColor = "red";
-        if (timeLeft > 0) {
-            let timeInterval = setInterval(function () {
-                // As long as the `timeLeft` is greater than 1
-                if (timeLeft > 1) {
-                    // Set the `textContent` of `timerDiv` to show the remaining seconds
-                    timerDiv.textContent = timeLeft + ' seconds remaining';
-                } else if (timeLeft === 1) {
-                    // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-                    timerDiv.textContent = timeLeft + ' second remaining';
-                }
-            }, 1000);
-            if (questionIndex === questions.length) {
-                console.log("show results 2 happening");
-                timeLeft = null;
-                showResults();
-                //return;
-
-            } else {;
-                generateQuestion();
-            }
-
-        } else {
-            timeLeft = null;
-            showResults();
-        }
-    }
-
-}
-
-function showResults() {
-    resetArea();
-    timerDiv.textContent = "";
-    timeLeft = null;
-    quizQuestion.textContent = "You got " + amountRight + " questions right!";
-    storeResults();
-
-}
-
-function resetArea() {
-    //console.log("Clearing quiz area for next question")
-    while (quizChoices.firstChild) {
-        quizChoices.removeChild(quizChoices.firstChild);
-    }
-}
-
-function storeResults() {
-    console.log("preparing to store locally");
-    let submitButton = document.createElement("button");
-    let submitText = document.createElement("input");
-
-    submitButton.textContent = "submit";
-
-    submitInitials.appendChild(submitText);
-    submitInitials.appendChild(submitButton);
-
-    refreshPage();
-
-    submitButton.addEventListener("click", function () {
-        console.log("storing locally now");
-        let submitName = submitText.value.trim();
-        if(amountRight > highRight) {
-            let highName = submitName;
-            let highRight = amountRight;
-        }       
-        const userScores = {
-            initials: submitName,
-            rightQuestion: amountRight
-        };
-        console.log(submitName);
-        console.log(amountRight);
-        localStorage.setItem("1", JSON.stringify(userScores));
-        submitInitials.remove();
-    });
-}
-
-function displayScores() {
-    highScoreBtn.style.display = "none";
-    let headerScore = document.createElement("h2");
-    highScoreScreen.appendChild(headerScore);
-    headerScore.textContent = "High Scores";
-    let scoresBox = document.createElement("ul");
-    highScoreScreen.appendChild(scoresBox);
-    let scores = document.createElement("li");
-    scoresBox.appendChild(scores);
-    let scoreObject = JSON.parse(localStorage.getItem("1"));
-    //console.log(scoreObject);
-    scores.textContent = scoreObject.initials + ": " + scoreObject.rightQuestion + " questions right";
-}
-
-function refreshPage() {
-    refreshButton = document.createElement("button");
-    refreshButton.textContent = "start again";
-    timerDiv.appendChild(refreshButton)
-    refreshButton.addEventListener("click", function() {
-        window.location.reload();
-    })
-}
